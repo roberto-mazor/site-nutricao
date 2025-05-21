65c005943dea5c392f15ccb080bccbf2ef5a6190
roberto-mazor <mazor.estudo@gmail.com>
2025-05-20 22:11:12 -0300

    colocando menu em um arquivo html separado

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('roll', window.scrollY > 0)
-})
+});
+
+fetch('menu.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('menu').innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao Carregar o menu', error));
+
+    
